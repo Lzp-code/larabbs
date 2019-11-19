a60663520b9beaac2a74b847727f97a14c4fdfb5
Lzp-code <lu-zhipeng@foxmail.com>
2019-11-19 13:28:10 +0000

    jichu-page

--- app/helpers.php
+++ app/helpers.php
@@ -1,39 +1,6 @@
 <?php
 
-namespace App;
-
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-
-class User extends Authenticatable
-{
-    use Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+//将当前请求的路由器名称转换为css类名称，作用是允许我们针对某个页面做页面样式定制
+function route_class(){
+    return str_replace('.','-',Route::currentRouteName());
 }
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+{{--获取config/app.php里面的Locale--}}
+<html lang="{{app()->getLocale()}}">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token 为了方便前端的js脚本获取csrf令牌-->
+    <meta name="csrf-token" content="{{csrf_token()}}">
+
+    {{--继承此模板的页面，如果没有设置title的话，则title自动设置为LaraBBS--}}
+    <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
+
+    <!-- Styles 根据webpack.mix.js来生成css链接-->
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
+</head>
+
+<body>
+{{--route_class是我们在helpers.php中的辅助方法--}}
+<div id="app" class="{{ route_class() }}-page">
+
+    @include('layouts._header')
+
+    <div class="container">
+
+        @include('shared._messages')
+
+        @yield('content')
+
+    </div>
+
+    @include('layouts._footer')
+</div>
+
+<!-- Scripts -->
+<script src="{{ mix('js/app.js') }}"></script>
+</body>
+
+</html>
