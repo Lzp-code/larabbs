a0fafa0d60e62473a09a2895a21b0a95ddc2e547
Lzp-code <lu-zhipeng@foxmail.com>
2019-11-23 17:49:33 +0000

    3.5-email-verify

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -26,6 +26,7 @@ class Kernel extends HttpKernel
      *
      * @var array
      */
+
     protected $middlewareGroups = [
         'web' => [
             \App\Http\Middleware\EncryptCookies::class,
@@ -35,6 +36,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\EnsureEmailIsVerified::class,      // <<--- 只需添加这一行
         ],
 
         'api' => [
@@ -79,4 +81,7 @@ class Kernel extends HttpKernel
         \Illuminate\Routing\Middleware\SubstituteBindings::class,
         \Illuminate\Auth\Middleware\Authorize::class,
     ];
+
+
+
 }
