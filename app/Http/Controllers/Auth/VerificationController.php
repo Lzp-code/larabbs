8a6172c503834ad58a3f28cc4de5c566bb941410
Lzp-code <lu-zhipeng@foxmail.com>
2019-11-24 10:30:25 +0000

    3.6-ren-zheng

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -29,13 +29,14 @@ class VerificationController extends Controller
 
     /**
      * Create a new controller instance.
-     *
+     *构建函数里使用了三个中间件，并且使用了中间件简称
+     * 这些简称是在 app/Http/Kernel.php 中的$routeMiddleware设置的
      * @return void
      */
     public function __construct()
     {
-        $this->middleware('auth');
-        $this->middleware('signed')->only('verify');
-        $this->middleware('throttle:6,1')->only('verify', 'resend');
+        $this->middleware('auth');//所有控制器必须登录才能访问
+        $this->middleware('signed')->only('verify');//只有verify动作使用signed中间件进行认证
+        $this->middleware('throttle:6,1')->only('verify', 'resend');//'verify', 'resend'的频率限制为一分钟不可超过六次
     }
 }
--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
     public function root(){
-//        dd(\Auth::user()->hasVerifiedEmail());
+
+//        var_dume(\Auth::user()->hasVerifiedEmail()); //验证下当前登录的用户是否是已经认证过
         return view('pages.root');
     }
 }
