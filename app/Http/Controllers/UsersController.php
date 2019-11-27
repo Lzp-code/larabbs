848e6a8fd1a9075c507b8418c6c613dc633bf9e7
Lzp-code <lu-zhipeng@foxmail.com>
2019-11-27 15:32:38 +0000

    4.8-quan-xian

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,18 +10,24 @@ use App\Handlers\ImageUploadHandler;
 class UsersController extends Controller
 {
 
+    public function __construct(){
+        $this->middleware('auth',['except' => ['show']]);
+    }
+
 
     public function show(User $user){
         return view('users.show',compact('user'));
     }
 
     public function edit(User $user){
+        $this->authorize('update',$user);
         return view('users.edit',compact('user'));
     }
 
     //传参 UserRequest。这将触发表单请求类的自动验证机制，验证发生在 UserRequest 中，
     //并使用此文件中方法 rules() 定制的规则，只有当验证通过时，才会执行 控制器 update() 方法中的代码
     public function update(UserRequest $request, ImageUploadHandler $uploader,User $user){
+        $this->authorize('update',$user);
 
         $data = $request->all();
 
