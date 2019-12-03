656d150f104e5b40cd79cc2d3e70a693663479cb
Lzp-code <lu-zhipeng@foxmail.com>
2019-12-03 15:17:24 +0000

    6.1.1

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -6,9 +6,8 @@ use Illuminate\Foundation\Auth\User;
 
 class Topic extends Model
 {
-    protected $fillable = ['title', 'body', 'user_id',
-        'category_id', 'reply_count', 'view_count',
-        'last_reply_user_id', 'order', 'excerpt', 'slug'
+    protected $fillable = [
+        'title', 'body', 'category_id', 'excerpt', 'slug'
     ];
 
 
