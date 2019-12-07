3798ee7f8cbf45b6cd95967e4f5cc441aad0c3fc
Lzp-code <lu-zhipeng@foxmail.com>
2019-12-07 08:58:58 +0000

    6.3

--- routes/web.php
+++ routes/web.php
@@ -45,4 +45,7 @@ Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'crea
 //分类列表话题
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
+//上传图片
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
+
 
