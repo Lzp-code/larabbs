5889023615bce5c213e88051eca4175725609b6f
Lzp-code <lu-zhipeng@foxmail.com>
2019-12-17 14:21:01 +0000

    7.4.1

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,9 +11,14 @@ class User extends Authenticatable implements MustVerifyEmailContract
 {
     //use以后，即可直接使用里面的方法
     use  MustVerifyEmailTrait;
+
+    //这里的意思是给 Notifiable 这个 trait 中的 notify 方法起一个别名 laravelNotify
+    //同时访问控制为受保护的（protected ）。
     use Notifiable {
         notify as protected laravelNotify;
     }
+
+//    use Notifiable, MustVerifyEmailTrait;
 
     protected $fillable = [
         'name', 'email', 'password','introduction','avatar',
@@ -37,18 +42,24 @@ class User extends Authenticatable implements MustVerifyEmailContract
         return $this->hasMany(Reply::class);
     }
 
-    public function notify($instance)
+    public function notify($instance,$create_uid)
     {
+        var_dump($create_uid);
+        var_dump($instance);
+
+        $instance->user_id = $create_uid;
+
+        var_dump($instance);
+        exit();
+
         // 如果要通知的人是当前用户，就不必通知了！
-        if ($this->id == Auth::id()) {
+        if ($create_uid == Auth::id()) {
             return;
         }
-
         // 只有数据库类型通知才需提醒，直接发送 Email 或者其他的都 Pass
         if (method_exists($instance, 'toDatabase')) {
             $this->increment('notification_count');
         }
-
         $this->laravelNotify($instance);
     }
 
--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,10 +3,10 @@
 namespace App\Observers;
 
 use App\Models\Reply;
+use App\Notifications\TopicReplied;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
-use App\Notifications\TopicReplied;
 
 class ReplyObserver
 {
@@ -15,8 +15,12 @@ class ReplyObserver
         $reply->topic->reply_count = $reply->topic->replies->count();
         $reply->topic->save();
 
+//        var_dump($reply->topic);
+//        var_dump($reply->topic->user_id);
+//        exit();
+        $create_uid = $reply->topic->user_id;
         // 通知话题作者有新的评论
-        $reply->topic->user->notify(new TopicReplied($reply));
+        $reply->user->notify(new TopicReplied($reply),$create_uid);
     }
 
     public function creating(Reply $reply)
